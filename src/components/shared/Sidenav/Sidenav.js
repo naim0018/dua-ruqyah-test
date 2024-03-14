8923b8e84dc28d7b91ec59b86447493be59586dd
naim0018 <mdkazinaim0018@gmail.com>
2024-03-14 23:34:42 +0600

    update

---
+++ src/components/shared/Sidenav/Sidenav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from "next/image";
+import logo from "../../../assets/logo.png"
+const Sidenav = () => {
+  return (
+    <div>
+     
+       <Image 
+        src={logo}
+      />
+    </div>
+  )
+}
+
+export default Sidenav
